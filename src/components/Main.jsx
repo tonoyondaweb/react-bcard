37f84db27f33bf1160580573011a5f13c831c206
tanay <iamtanaylodh@gmail.com>
2022-09-03 23:32:38 +0530

    added main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,15 @@ import React from "react"
 
 export default function Main() {
     return (
-        <h1>This is Main Content</h1>
+        <main className="main">
+            <h2>About</h2>
+            <p>
+                I am a B.Tech Computer Sciences graduated and a frontend developer starting to get my hands on react. I try and learn more everyday, keep up with the best practices across all my work and always look for a new challenge.
+            </p>
+            <h2>Interests</h2>
+            <p>
+                Food, music, geopolitics, political history, culture, tech.
+            </p>
+        </main>
     )
 }
